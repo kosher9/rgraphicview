9d8835dabd9a694962bc1de5882f387fb0b4f149
kosher9 <lionel.kossi@gmail.com>
2023-02-08 16:36:06 +0000

    Add some comments

--- lib/rgraphicview.rb
+++ lib/rgraphicview.rb
@@ -5,4 +5,7 @@ require_relative "rgraphicview/version"
 module Rgraphicview
   class Error < StandardError; end
   # Your code goes here...
+  # Been able to generate an html file with it's css
+  # Been able to get in app folder and navigate in it.
+  # Have an idea of what you want your canvas to look like
 end
