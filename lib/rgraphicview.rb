f9408cf2817ca23ba51e3578abca8e8f3f8f2f6b
kosher9 <lionel.kossi@gmail.com>
2023-02-14 17:03:15 +0000

    Generate html

--- lib/rgraphicview.rb
+++ lib/rgraphicview.rb
@@ -8,4 +8,57 @@ module Rgraphicview
   # Been able to generate an html file with it's css
   # Been able to get in app folder and navigate in it.
   # Have an idea of what you want your canvas to look like
+  class HtmlGenerator
+    def initialize(title, stylesheet)
+      @title = title
+      @stylesheet = stylesheet
+    end
+
+    def generate(output_dir)
+      Dir.mkdir(output_dir) unless File.exists?(output_dir)
+
+      File.open(File.join(output_dir, 'index.html'), 'w') do |f|
+        f.write(ERB.new(html_template).result(binding))
+      end
+
+      File.open(File.join(output_dir, 'style.css'), 'w') do |f|
+        f.write(css_template)
+      end
+    end
+
+    private
+
+    def html_template
+      <<-TEMPLATE
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title><%= @title %></title>
+            <link rel="stylesheet" type="text/css" href="style.css">
+          </head>
+          <body>
+            <h1><%= @title %></h1>
+            <p>Hello, World!</p>
+          </body>
+        </html>
+      TEMPLATE
+    end
+
+    def css_template
+      <<-TEMPLATE
+        body {
+          background-color: #f0f0f0;
+          font-family: Arial, sans-serif;
+        }
+
+        h1 {
+          color: #333;
+        }
+
+        p {
+          font-size: 1.2em;
+        }
+      TEMPLATE
+    end
+  end
 end
